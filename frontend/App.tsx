f0b23126a7abf16daea960c0b965f3c621671c93
Hoang Huong | apikey:d0851193d83d8c0dc0833e050f2c432f59dd585145eb42c2440363789246f383 <huong.ph@fsofts.com>
2025-03-15 15:53:16 +0700

    save

---
+++ frontend/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import HomeScreen from './src/screens/HomeScreen';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
+
+
+function App(): React.JSX.Element {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+export default App;
---
+++ frontend/src/utils/http.ts
@@ -0,0 +1,2 @@
+const ROOT_HTTP = 'http://10.0.2.2:5000/api/';
+export default {ROOT_HTTP};
